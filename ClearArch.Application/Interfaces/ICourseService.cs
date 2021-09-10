c6511e6e08bc754307b6fd32438cc0f3824fc58b
furkan.sakizci <furkan.sakizci@WISGNDTYAZ15>
2021-09-10 14:41:51 +0300

    Add project files.

---
+++ ClearArch.Application/Interfaces/ICourseService.cs
@@ -0,0 +1,12 @@
+﻿using ClearArch.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClearArch.Application.Interfaces
+{
+    public interface ICourseService
+    {
+        public CourseViewModel GetCourses();
+    }
+}
